7151e5a32d3c9c02439fdd74683cf566d0400938
w3scools-test <hliasxristou2013@hotmail.com>
2023-01-05 15:04:10 +0200

    Add project files.

---
+++ Order IDs belonging by a specific letter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Order_IDs_belonging_by_a_specific_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] orderID = new string[8] { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+
+
+            foreach (string ID in orderID)
+
+            {
+                if (ID.StartsWith("B"))
+
+                {
+                    Console.WriteLine(ID);
+                }
+            }
+
+        }
+    }
+}
